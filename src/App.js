831b85c3bb83eded25fad5fdb74dd1c7a9daa133
Miley Wright <dishaliekwright@yahoo.com>
2020-11-03 01:38:00 -0500

    import images

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={`${process.env.PUBLIC_URL}/assets/images/skin_product_pm.webp`} alt='hey'/>
     </div>
   );
 }
