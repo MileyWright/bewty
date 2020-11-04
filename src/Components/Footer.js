2fc44d5d5bc9411f169531031c471daff6105ad2
Miley Wright <dishaliekwright@yahoo.com>
2020-11-04 00:00:56 -0500

    Footer complete

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,49 @@
 const Footer = () => {
     return (
         <>
-            <div>
-                Footer
+            <div className='footer'>
+                <div className='footer_col'>
+                    <p className='footer_title'>Help</p>
+                    <p>Have A Question?</p>
+                    <p>Privacy Policy</p>
+                    <p>Press Inquiries?</p>
+                    <div>
+                        <a href="mailto:contact@bewty.com"> contact@bewty.com</a>
+                    </div>
+                </div>
+                <div className='footer_col'>
+                    <p className='footer_title'>Shop</p>
+                    <p>Primary Care</p>
+                    <p>Hair</p>
+                    <p>Skin</p>
+                    <p>Supplements</p>
+                    <p>Online Therapy</p>
+                    <p>COVID-19 Test</p>
+                </div>
+                <div>
+                    <p className='footer_title'>Learn</p>
+                    <p>About Us</p>
+                    <p>Our Medical Experts</p>
+                    <p>Careers</p>
+                    <p>Savoir Faire</p>
+                    <p>How It Works</p>
+                    <p>Purpose</p>
+                    <p>Reviews</p>
+                    <p>Medical Conditions</p>
+                    <p>Investors</p>
+                </div>
+                <div>
+                    <p className='footer_title'>Popular</p>
+                    <p>Cialis</p>
+                    <p>Finasteride</p>
+                    <p>Minoxidil</p>
+                    <p>Slidenafil</p>
+                    <p>Tadalafil</p>
+                    <p>Sertraline For PE</p>
+                    <p>Propranolol</p>
+                </div>
             </div>
+            <p className='copyright'>© Miley Wright 2020. All Rights Reserved.</p>
         </>
     )
 }
